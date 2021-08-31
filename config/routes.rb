d5e3a1ec0bfee515663733a4a18e80e7c6943ee1
Alex <alex830216@gmail.com>
2021-08-31 14:03:04 +0800

    用shallow_nesting建立comment_routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get "/", to: "notes#index"
   
-  resources :notes
+  resources :notes do
+    resources :comments, shallow: true, except: [:new, :edit, :update]
+  end
 end
