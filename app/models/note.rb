bc487204cf02a3957376e9ecef0c2d668a65a697
Alex <alex830216@gmail.com>
2021-08-31 13:54:01 +0800

    建立comment_model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,10 +1,9 @@
 class Note < ApplicationRecord
     
-	belongs_to :user
+	# belongs_to :user
 	has_many :comments
 	has_many :tags
 	has_many :likes
 	has_many :collections
 	has_many :subscribes
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_044848) do
+ActiveRecord::Schema.define(version: 2021_08_31_054126) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "content"
+    t.integer "note_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["note_id"], name: "index_comments_on_note_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
@@ -36,5 +46,6 @@ ActiveRecord::Schema.define(version: 2021_08_31_044848) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "notes", "users"
+  add_foreign_key "comments", "notes"
+  add_foreign_key "comments", "users"
 end
