8e531705edf4cf31190cc145933b5bae925319ad
m83115 <m83115@yahoo.com.tw>
2021-09-02 17:47:28 +0800

    addFavorite p2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,8 +19,6 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import ax from "axios"
-
 document.addEventListener('turbolinks:load', function () {  // 因為其他html沒有 <div id="editor"></div>，所以其他頁會出錯，這頁有，但因為turbolinks的一些問題，所以要等turbolinks跑完再判斷有無抓到元素，有的話就執行
   let aa = document.querySelector('#editor')
 
@@ -45,7 +43,7 @@ function addFavorite(id) {
   const url = `/api/v1/notes/${id}/favorite`;
   ax.post(url)
     .then((res) => {
-      const icon = document.querySelector(".favorite_icon");
+      const icon = document.querySelector("#favorite_icon");
       if (res.data.status === "added") {
         icon.classList.remove("favorite-off");
         icon.classList.add("favorite-on");
