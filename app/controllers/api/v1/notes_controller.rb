507da008db043fc7fc4dfffff7f5582647918dac
m83115 <m83115@yahoo.com.tw>
2021-09-02 16:14:15 +0800

    controller api

---
+++ app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::NotesController < ApplicationController
+    before_action :check_login!  
+    # POST /api/v1/notes/2/favorite
+    def favorite
+      note = Note.find(params[:id])
+      if Bookmark.exists?(note: note)
+        #移除最愛
+        current_user.favorite_notes.delete(note)
+        render json: { status: "removed",id: params[:id] }
+      else
+        #新增最愛  
+        current_user.favorite_notes << note
+        render json: { status: "added",id: params[:id] }
+      end
+      
+    end    
+end
