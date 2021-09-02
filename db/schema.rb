6dd9c50203ffc186a5def464cc068453fa7bc5c5
m83115 <m83115@yahoo.com.tw>
2021-09-02 16:14:15 +0800

    controller api

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2021_09_06_044724) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["note_id"], name: "index_comments_on_note_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+ActiveRecord::Schema.define(version: 2021_09_02_072412) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "note_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["note_id"], name: "index_likes_on_note_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -56,9 +65,17 @@ ActiveRecord::Schema.define(version: 2021_09_06_044724) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+<<<<<<< HEAD
   add_foreign_key "comments", "notes"
   add_foreign_key "comments", "users"
+<<<<<<< HEAD
   add_foreign_key "likes", "notes"
   add_foreign_key "likes", "users"
+=======
+=======
+  add_foreign_key "likes", "notes"
+  add_foreign_key "likes", "users"
+>>>>>>> 0f0fb39 (controller api)
+>>>>>>> ce014b6 (controller api)
   add_foreign_key "notes", "users"
 end
