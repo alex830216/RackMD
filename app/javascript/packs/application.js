d4adc9d8cde5a242b1e7d5d40e56e0a49c108dcd
m83115 <m83115@yahoo.com.tw>
2021-09-02 16:21:16 +0800

    addFavorite p1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,10 @@ ActiveStorage.start()
 
 document.addEventListener('turbolinks:load', function () {
   let editorSelected = document.querySelector('#editor')
+import ax from "axios"
+
+document.addEventListener('turbolinks:load', function () {  // 因為其他html沒有 <div id="editor"></div>，所以其他頁會出錯，這頁有，但因為turbolinks的一些問題，所以要等turbolinks跑完再判斷有無抓到元素，有的話就執行
+  let aa = document.querySelector('#editor')
 
   if (editorSelected) {
 
@@ -32,4 +36,30 @@ document.addEventListener('turbolinks:load', function () {
     });
   }
 })
+
+import "controllers"
+function addFavorite(id) {
+  const token = document.querySelector("meta[name=csrf-token]").content;
+  ax.defaults.headers.common["X-CSRF-Token"] = token;
+
+  const url = `/api/v1/notes/${id}/favorite`;
+  ax.post(url)
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+}
+
+document.addEventListener("turbolinks:load", () => {
+  const btn = document.querySelector("#favorite_btn");
+
+  if (btn) {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      addFavorite(e.currentTarget.dataset.id);
+    });
+  }
+});
 
