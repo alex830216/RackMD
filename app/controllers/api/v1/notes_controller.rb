976b0a8995fe984141113db4bbff2c08440899c5
m83115 <m83115@yahoo.com.tw>
2021-09-03 15:56:51 +0800

    修改權限/CSS

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::NotesController < ApplicationController
 <<<<<<< HEAD
+<<<<<<< HEAD
   before_action :authenticate_user!
 
   def collection
@@ -17,17 +18,21 @@ class Api::V1::NotesController < ApplicationController
 end
 =======
     before_action :check_login!  
+=======
+    before_action :authenticate_user!  
+>>>>>>> 5a78d03 (修改權限/CSS)
     # POST /api/v1/notes/2/favorite
+
     def favorite
       note = Note.find(params[:id])
       if Like.exists?(note: note)
         #移除最愛
         current_user.favorite_notes.delete(note)
-        render json: { status: "removed",id: params[:id] }
+        render json: { status: "removed", id: params[:id] }
       else
         #新增最愛  
         current_user.favorite_notes << note
-        render json: { status: "added",id: params[:id] }
+        render json: { status: "added", id: params[:id] }
       end
       
     end    
