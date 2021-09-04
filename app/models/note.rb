1fcc1ab8659445e7b24b916febc019249b80a7e6
sarah <sarahlin280@gmail.com>
2021-09-04 16:59:33 +0800

    add relation to tag and note

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,8 @@ class Note < ApplicationRecord
 	paginates_per 3
 	belongs_to :user
 	has_many :comments
-	has_many :tags
+	has_many :taggings
+	has_many :tags, through: :taggings
 	has_many :likes
 	has_many :collections
 	has_many :subscribes
