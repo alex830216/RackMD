b4887d93f07de5a6328361e8d7dcac31bfea6254
Alex <alex830216@gmail.com>
2021-09-05 20:03:59 +0800

    remove needless

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  purge: [],
-  darkMode: false,
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
   plugins: [
     require("tailwindcss")("./app/javascript/stylesheets/tailwind.config.js"),
     require('postcss-import'),
