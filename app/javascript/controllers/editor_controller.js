aae8ec4d8e16c4d14c505dc09912818327d7f8d1
yuchan <lumir1031@gmail.com>
2021-09-05 17:33:40 +0800

    example

---
+++ app/javascript/controllers/editor_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "stimulus"
+import axios from 'axios'
+
+export default class extends Controller {
+  static targets = [ "name" ]
+  static values = { id: Number }
+
+  updateValue() {
+    const csrfToken = document.querySelector("meta[name=csrf-token]").content;
+    axios.defaults.headers.common["X-CSRF-Token"] = csrfToken;
+
+    // 找到 markdown 的內容
+    const content = document.querySelector('.ProseMirror').innerHTML
+
+    // .toastui-editor-contents 這個 class 會渲染出 html，因此在這裡找標題
+    const editorContent = document.querySelector('.toastui-editor-contents')
+    // 確定第一行是不是 h1 格式
+    const title = editorContent.firstChild.nodeName === 'H1' ? editorContent.firstChild.innerHTML : 'Untitle'
+
+    let data = { 'note': { title, content }}
+
+    axios.put(`/notes/${this.idValue}`, data)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(error => console.error(error));
+  }
+
+
+  get name(){
+    return this.nameTarget.value
+  }
+
+
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,39 +7,25 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-// import axios from 'axios'
+import "controllers"
 import Editor from '@toast-ui/editor';
-
-
 import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
-
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-document.addEventListener('turbolinks:load', function () {  // 因為其他html沒有 <div id="editor"></div>，所以其他頁會出錯，這頁有，但因為turbolinks的一些問題，所以要等turbolinks跑完再判斷有無抓到元素，有的話就執行
-  let aa = document.querySelector('#editor')
+document.addEventListener('turbolinks:load', function () {
 
-if (aa) {
+  let editor = document.querySelector('#editor')
 
-  const editor = new Editor({
-    el: document.querySelector('#editor'),
-    height: '600px',
-    initialEditType: 'markdown',
-    previewStyle: 'vertical'
-  });
-}
-})
-
-import "controllers"
-
-// const getUsers = () => {
-//   axios.get('/notes/new')
-//   .then(response => {
-//     const users = response.data.data;
-//     console.log(`GET user`, users);
-//   })
-//   .catch(error => console.error(error));
-// };
-// getUsers();
+  if (editor) {
+    const editor = new Editor({
+      el: document.querySelector('#editor'),
+      height: '600px',
+      initialEditType: 'markdown',
+      previewStyle: 'vertical',
+      initialValue: 'omgggg' // 要打 API
+    });
+  }
+  })
