b050f430f995a63dc57509eb0f4466774b6bc57a
YiHua <helen904526@gmail.com>
2021-09-06 21:21:12 +0800

    find out tags list

---
+++ app/javascript/controllers/addTags_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "editor" ]
+  // let targetInsert;
+  connect() {
+    console.log("addTags controller connect!!")
+    this.findTags();
+  }
+
+  findTags() {
+    console.log("find Tags !!")
+    let targetInsert = setInterval( () => {
+      console.log("Interval!")
+      const tagNodes = this.editorTarget.querySelectorAll(".toastui-editor-md-heading6 .toastui-editor-md-marked-text")
+      const tags = Object.values(tagNodes).map((node) => node.innerText)
+      console.log(tags)
+      // 來打ＡＰＩ
+
+    }, 5000);
+
+    // this.editorTarget.addEventListener('keyup', function(e){
+    //   // enter 的 keyCode 是 13
+    //   if( e.keyCode === 13 ){
+    //     console.log("Enter up!!");
+    //     console.log(this.editorTarget.querySelector(".toastui-editor-md-heading6"))
+    //   }
+
+    // }, false);
+
+
+
+
+  // function stopFindTags() {
+  //   clearInterval(targetInsert)
+  // }
+    // console.log(this.editorTarget.querySelector(".toastui-editor-md-heading6"))
+    // if (this.editorTarget.querySelector(".toastui-editor-md-heading6").innerText = " tags: "){
+    //   console.log("yes!!")
+    // }
+  }
+}
