2dfa5686b8847a97cc4eb1bc85c5c470baa1fb36
m83115 <m83115@yahoo.com.tw>
2021-09-06 12:00:46 +0800

    解衝突

--- db/schema.rb
+++ db/schema.rb
@@ -1,21 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_09_06_101028) do
-=======
-ActiveRecord::Schema.define(version: 2021_09_01_100714) do
->>>>>>> 02977e8fcda45da261faf3856e05a4562d77eff4
 
   create_table "collections", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -25,7 +8,6 @@ ActiveRecord::Schema.define(version: 2021_09_01_100714) do
     t.index ["note_id"], name: "index_collections_on_note_id"
     t.index ["user_id"], name: "index_collections_on_user_id"
   end
-<<<<<<< HEAD
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -35,9 +17,6 @@ ActiveRecord::Schema.define(version: 2021_09_01_100714) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["note_id"], name: "index_comments_on_note_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-=======
->>>>>>> 0f0fb39 (controller api)
-ActiveRecord::Schema.define(version: 2021_09_02_072412) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -47,11 +26,6 @@ ActiveRecord::Schema.define(version: 2021_09_02_072412) do
     t.index ["note_id"], name: "index_likes_on_note_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
-<<<<<<< HEAD
-=======
->>>>>>> 02977e8fcda45da261faf3856e05a4562d77eff4
-=======
->>>>>>> 0f0fb39 (controller api)
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
@@ -93,27 +67,12 @@ ActiveRecord::Schema.define(version: 2021_09_02_072412) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   add_foreign_key "collections", "notes"
   add_foreign_key "collections", "users"
-=======
->>>>>>> 7ddc41b (controller api)
-<<<<<<< HEAD
   add_foreign_key "comments", "notes"
   add_foreign_key "comments", "users"
-=======
-<<<<<<< HEAD
->>>>>>> 02977e8fcda45da261faf3856e05a4562d77eff4
-=======
   add_foreign_key "likes", "notes"
   add_foreign_key "likes", "users"
->>>>>>> 0f0fb39 (controller api)
->>>>>>> 7ddc41b (controller api)
-=======
-  add_foreign_key "likes", "notes"
-  add_foreign_key "likes", "users"
->>>>>>> 0f0fb39 (controller api)
   add_foreign_key "notes", "users"
   add_foreign_key "taggings", "notes"
   add_foreign_key "taggings", "tags"
