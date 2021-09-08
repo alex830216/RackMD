c3b48a6a56d698d58d772c424d1aae3489e79f1c
YiHua <helen904526@gmail.com>
2021-09-08 21:33:31 +0800

    add filter before tag create

--- app/javascript/controllers/addTags_controller.js
+++ app/javascript/controllers/addTags_controller.js
@@ -5,38 +5,30 @@ import axios from "axios";
 
 export default class extends Controller {
   static targets = [ "editor" ]
+  static values = { id: Number }
 
   connect() {
     this.findTags();
   }
 
   findTags() {
-    let targetInsert = setTimeout( () => {
+    var targetInsert = setInterval( () => {
       const tagNodes = this.editorTarget.querySelectorAll(".toastui-editor-md-heading6 .toastui-editor-md-marked-text")
-      const tags = Object.values(tagNodes).map((node) => node.innerText)
-      
+      const tags = Object.values(tagNodes).map((node) => {
+        const tagText = node.innerText
+        return this.filter(tagText)
+      })
       const csrfToken = document.querySelector("meta[name=csrf-token]").content;
       axios.defaults.headers.common["X-CSRF-Token"] = csrfToken;
       const url = `/api/v1/notes/${this.idValue}/tag`
       axios.post(url, { tag_str: tags.toString() })
-           .then((res) => console.log(`success!!`))
-           .catch((err) => console.log(`error!${err}`))
-    }, 10000);
+      .then((res) => console.log(`success!!`))
+      .catch((err) => console.log(`error!${err}`))
+    }, 15000);
   }
-
-    // this.editorTarget.addEventListener('keyup', function(e){
-    //   // enter 的 keyCode 是 13
-    //   if( e.keyCode === 13 ){
-    //     console.log("Enter up!!");
-    //     console.log(this.editorTarget.querySelector(".toastui-editor-md-heading6"))
-    //   }
-    // }, false);
-
-  // function stopFindTags() {
-  //   clearInterval(targetInsert)
-  // }
-    // console.log(this.editorTarget.querySelector(".toastui-editor-md-heading6"))
-    // if (this.editorTarget.querySelector(".toastui-editor-md-heading6").innerText = " tags: "){
-    //   console.log("yes!!")
-    // }
+  
+  filter(str) { 
+    var pattern=/[`~ !@#$^&*()=|{}':;',\\\[\]\.<>\/?~！@#￥……&*（）——|{}【】'；：""'。，、？]/g;
+    return str.replace(pattern,"");
+  } 
 }
