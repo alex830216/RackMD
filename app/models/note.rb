1c19f97d535449a58fad1351a1309b66b327becd
sarah <sarahlin280@gmail.com>
2021-09-09 18:14:03 +0800

    add dependent: :destroy

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,27 +4,28 @@ class Note < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :collections
   has_many :subscribes
 
   # tag_list 的 getter
   def tag_list
-	tags.map(&:title).join(', ')
+	  tags.map(&:title).join(', ')
   end
 
 	# tag_list 的 setter
   def tag_list=(title)
-	self.tags = title.split(',').map do |item|
-	Tag.where(title: item.strip).first_or_create!
-	end
+	  self.tags = title.split(',').map do |item|
+	  Tag.where(title: item.strip).first_or_create!
+	  end
   end
 	
   def self.search(search) 
     if search
-	  where(['title LIKE ?', "%#{search}%"]) 
-	else
-	  all 
-	end	
+	    where(['title LIKE ?', "%#{search}%"]) 
+	  else
+	    all 
+	  end	
   end
+    
 end
