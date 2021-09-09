bb391d8bdff2bcf6887a9f5cf29a225dd6709b3d
Wang Sheng i <86760537+m83115@users.noreply.github.com>
2021-09-09 11:53:57 +0800

    Revert "新增按讚功能"

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   get "/", to: "notes#index"
   get "/users/collections", to: "users/collections#index"
   
-<<<<<<< HEAD
   resources :notes do
     resources :comments, shallow: true, except: [:new] 
   end
-<<<<<<< HEAD
 
   
 
@@ -22,22 +20,4 @@ Rails.application.routes.draw do
       end
     end
   end
-  
-=======
-  resources :notes
->>>>>>> 0f0fb39 (controller api)
-=======
-  
->>>>>>> d0202ec5039bf15862c7eabf5edc6d14228d38b7
-  # API
-  # POST /api/v1/notes/2/favorite
-  namespace :api do
-    namespace :v1 do
-      resources :notes, only: [] do
-        member do
-          post :favorite
-        end  
-      end
-    end 
-  end   
 end
