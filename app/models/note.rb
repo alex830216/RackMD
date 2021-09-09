48b81eadb6fbd7633fbaad74647e12f5218c5164
sarah <sarahlin280@gmail.com>
2021-09-09 19:19:39 +0800

    縮排修正

--- app/models/note.rb
+++ app/models/note.rb
@@ -10,22 +10,22 @@ class Note < ApplicationRecord
 
   # tag_list 的 getter
   def tag_list
-	  tags.map(&:title).join(', ')
+    tags.map(&:title).join(', ')
   end
 
-	# tag_list 的 setter
+  # tag_list 的 setter
   def tag_list=(title)
-	  self.tags = title.split(',').map do |item|
-	  Tag.where(title: item.strip).first_or_create!
-	  end
+    self.tags = title.split(',').map do |item|
+    Tag.where(title: item.strip).first_or_create!
+    end
   end
-	
+  
   def self.search(search) 
     if search
-	    where(['title LIKE ?', "%#{search}%"]) 
-	  else
-	    all 
-	  end	
+      where(['title LIKE ?', "%#{search}%"]) 
+    else
+      all 
+    end	
   end
     
 end
