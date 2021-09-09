20561d64ccf1155d3b8463a1fd42f05e6f4bec91
john <t69685186@gmail.com>
2021-09-09 15:19:33 +0800

    新增收藏搜尋功能

--- app/controllers/users/collections_controller.rb
+++ app/controllers/users/collections_controller.rb
@@ -2,7 +2,7 @@ class Users::CollectionsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @collections = current_user.collected_notes.order(updated_at: :desc).page(params[:page])
+    @collections = current_user.collected_notes.order(updated_at: :desc).search(params[:search]).page(params[:page])
   end
 
 end
