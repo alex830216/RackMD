47d79987d4fa2f707fffd50eaed00c5b679fac41
Alex <alex830216@gmail.com>
2021-09-12 11:12:39 +0800

    btn toggle

--- app/javascript/controllers/commentToggle_controller.js
+++ app/javascript/controllers/commentToggle_controller.js
@@ -1,12 +1,16 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "display" ]
+  static targets = [ "display", "btn" ]
   toggle() {
     if (this.displayTarget.style.display === "none") {
       this.displayTarget.style.display = "block"
     } else {
       this.displayTarget.style.display = "none"
     }
+  }
+  hide(e) {
+    e.preventDefault()
+    this.displayTarget.style.display = "none"
   }
 }
