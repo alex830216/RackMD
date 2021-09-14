8d704d8da8089a5c974cb2772ff0434f3b717080
YiHua <helen904526@gmail.com>
2021-09-15 01:23:45 +0800

    code refactoring

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -8,6 +8,15 @@ module NotesHelper
     content_tag(:i, '', class: ["fa-heart", "favorite_icon", shape], data: {'favorite-target': 'icon' })
   end
 
+  def collection_icon(user, note)
+    if user && user.collection?(note)
+      shape = 'fas'
+    else
+      shape = 'far'
+    end
+    content_tag(:i, '', class: ["fa-bookmark", "collection_icon", shape], data: {'collect-target': 'icon' })
+  end
+
   def day_difference(note)
     t = Time.now
     (( t - note.updated_at) / 1.day ).to_i
