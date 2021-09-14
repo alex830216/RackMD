18e142a2f3a60cf2380fb08649bb6519b7c3b9ca
YiHua <helen904526@gmail.com>
2021-09-14 12:31:59 +0800

    convert to debounce in JS -WIP

--- app/javascript/controllers/addTags_controller.js
+++ app/javascript/controllers/addTags_controller.js
@@ -8,31 +8,50 @@ export default class extends Controller {
   static values = { id: Number }
 
   connect() {
-    this.findTags();
+    console.log("connect!!")
   }
 
-  findTags() {
-    var catchTag = setInterval( () => {
+  findTags(e) {
+    console.log("hi!")
+    var catchTag = () => {
       const tagNodes = this.editorTarget.querySelectorAll(".toastui-editor-md-heading6 .toastui-editor-md-marked-text")
-      const tags = Object.values(tagNodes).map((node) => {
+
+      var tags = Object.values(tagNodes).map((node) => {
         const tagText = node.innerText
         return filter(tagText)
       })
+      return tags
+    }
+    var saveTag = debounce(() => {
+      catchTag()
       const csrfToken = document.querySelector("meta[name=csrf-token]").content;
       axios.defaults.headers.common["X-CSRF-Token"] = csrfToken;
       const url = `/api/v1/notes/${this.idValue}/tag`
       axios.post(url, { tag_str: tags.toString() })
       .then((res) => console.log(`success!!`))
       .catch((err) => console.log(`error!${err}`))
-    }, 15000);
-    document.addEventListener("turbolinks:load", () => {
-      document.addEventListener("beforeunload", () => {
-        clearInterval(catchTag)
-      })
-    });
+    }, 1000)
+
+    if(e.keyCode !== undefined){
+      saveTag()
+      console.log("save!!")
+    }
+
     function filter(str) { 
       var pattern=/[`~ !@#$^&*()=|{}':;',\\\[\]\.<>\/?~！@#￥……&*（）——|{}【】'；：""'。，、？]/g;
       return str.replace(pattern,"")
+    }
+
+    function debounce(func, delay) {
+      var timer = null;
+      return function () {
+        var context = this;
+        var args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+          func.apply(context, args)
+        }, delay);
+      }
     }
   }
 }
