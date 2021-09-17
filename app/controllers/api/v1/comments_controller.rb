c0011a3ae1d942073da9864d1951acdba2f2a443
Alex <alex830216@gmail.com>
2021-09-17 13:58:32 +0800

    排版

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -6,8 +6,8 @@ class Api::V1::CommentsController < ApplicationController
   end
 
   private
+
   def comment_params
-    params.require(:comment)
-          .permit(:content)
+    params.require(:comment).permit(:content)
   end
 end
