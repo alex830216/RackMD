fc61482556fa34b653615699f9c682a182893ce8
YiHua <helen904526@gmail.com>
2021-09-17 09:12:39 +0800

    add CSS to editor -WIP

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  corePlugins: {
-  },
+  corePlugins: {},
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       outline: {
-          gray: ['1px solid #dddddd'],
+        gray: ["1px solid #dddddd"],
       },
       colors: {
         gray: {
-          lightest_xs: '#f8f8f8',
-          lightest_s: '#6e6e6e',
-          lightest: '#444444',
-          light: '#969696',
-          DEFAULT: '#333333',
-          dark: '#262626',
-          deep: '#222222',
-          
+          lightest_xs: "#f8f8f8",
+          lightest_s: "#6e6e6e",
+          lightest: "#444444",
+          light: "#969696",
+          DEFAULT: "#333333",
+          dark: "#262626",
+          deep: "#222222",
+          deepest: "#1e1e1e",
         },
-        white: '#ffffff',
-        green: '#14c053',
-        primary: '',
-        secondary: '',
+        white: "#ffffff",
+        green: "#14c053",
+        primary: "",
+        secondary: "",
+        editor: "#1d2127",
       },
     },
   },
