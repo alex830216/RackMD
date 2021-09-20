66e0e23c5d72d237ad1c7659b40bcb142678a40e
Alex <alex830216@gmail.com>
2021-09-20 20:36:58 +0800

    修正縮排

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,11 +1,11 @@
 class Collection < ApplicationRecord
   belongs_to :user
   belongs_to :note
-  def self.search(search) 
-	  if search
-	    where(['title LIKE ?', "%#{search}%"]) 
-	  else
-	    all 
-	  end
-	end
+  def self.search(search)
+		if search
+			where(['title LIKE ?', "%#{search}%"])
+		else
+			all 
+    end
+  end
 end
