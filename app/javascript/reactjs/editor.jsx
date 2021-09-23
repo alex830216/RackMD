4d7a236fa15d7a3f8f95ec63038e30fbe97724c5
sarah <sarahlin280@gmail.com>
2021-09-23 13:10:46 +0800

    resolve conflict

--- app/javascript/reactjs/editor.jsx
+++ app/javascript/reactjs/editor.jsx
@@ -1,60 +1,86 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import marked from "marked";
-import hljs from 'highlightjs';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+import 'prismjs/themes/prism-okaidia.css';
 
+function BlankCodeBlock({ value }) {
+	return <pre className="language-">{value || ''}</pre>;
+}
 
-marked.setOptions({
-  pedantic: false,
-  gfm: true,
-  breaks: true,
-  sanitize: false,
-  smartLists: true,
-  smartypants: false,
-  xhtml: true,
-  highlight: function (code) {
-    hljs.highlightAuto(code).value;
-  }
-})
+function CodeBlock({ language, value }) {
+	// 1. no language was typed
+	// or 2. language doesnt exist
+	if (!language || !Prism.languages[language] || !value)
+		return <BlankCodeBlock value={value} />;
 
-const renderer = new marked.Renderer();
+	var html = Prism.highlight(value, Prism.languages[language]);
+	var cls = 'language-' + language;
 
-hljs.configure({
-tabReplace: '  ',
-classPrefix: 'hljs-',
-languages: ['CSS', 'HTML, XML', 'JavaScript', 'PHP', 'Python', 'Stylus', 'TypeScript', 'Markdown']
-})
+	return (
+		<pre className={cls}>
+			<code dangerouslySetInnerHTML={{ __html: html }} className={cls} />
+		</pre>
+	);
+}
+
 
 function App() {
-  const [text, setText] = React.useState("");
+	const [markdown, setMarkdown] = useState('# sup');
 
-  return(
-    <div>
-      <h1>My Markdown Preview</h1>
-      <textarea 
-        name="text" 
-        id="text" 
-        rows="10" 
-        value={text}
-        onChange={(e) => setText(e.target.value)} 
-        className="textarea">
-      </textarea>
-      <h2>Output</h2>
-      <Preview markdown={text} />
-    </div>
-  );
+	return (
+		<StyledApp>
+			<StyledEditor>
+				<textarea
+					onChange={e => setMarkdown(e.target.value)}
+					value={markdown}
+				/>
+			</StyledEditor>
+
+			<StyledPreview>
+				<ReactMarkdown children={markdown} components={{ code: CodeBlock }} />
+			</StyledPreview>
+		</StyledApp>
+	);
 }
 
-function Preview({ markdown }) {
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-      __html: marked(markdown, { renderer: renderer }),
-    }}
-    id="preview"
-    ></div>
-  );
-}
+const StyledApp = styled.div`
+	display: flex;
+	width: 100%;
+	height: 70vh;
+
+	div {
+		flex: 1;
+		padding: 10px;
+	}
+`;
+
+const StyledEditor = styled.div`
+	background: ;
+
+	textarea {
+		width: 100%;
+		padding: 5px;
+		height: 100%;
+		border-radius: 5px;
+		border: 1px solid #e2e2e2;
+    background: #333;
+    color: #ABB2BF;
+	}
+`;
+
+const StyledPreview = styled.div`
+	background: #f8f8f8;
+
+	pre {
+		background: #fff;
+		color: red;
+		padding: 30px;
+	}
+`;
+
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
