a600e948dd1cd6b6a8dbb2b579465e557cd803bb
m83115 <m83115@yahoo.com.tw>
2021-09-23 10:39:04 +0800

    code review fix

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -16,11 +16,9 @@ class Api::V1::NotesController < ApplicationController
   def favorite
     note = Note.find(params[:id])
     if Like.exists?(note: note)
-      #移除最愛
       current_user.favorite_notes.delete(note)
       render json: { status: "removed", id: params[:id] }
     else
-      #新增最愛  
       current_user.favorite_notes << note
       render json: { status: "added", id: params[:id] }
     end   
--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -1,15 +1,19 @@
 class Users::ProfilesController < ApplicationController
   def public_note
     @user = User.find(params[:id])
-    @notes = @user.notes.order(updated_at: :desc).search(params[:search]).page(params[:page])
+    # @notes = @user.notes.order(updated_at: :desc).search(params[:search]).page(params[:page])
+    @notes = Note.where(user_id: params[:id])
+                 .search(params[:search])
+                 .page(params[:page])  
   end  
+
   def private_note
     @user = User.find(params[:id])
-    @notes = @user.notes
+    # @notes = @user.notes
   end
+  
   def like_note
     @user = User.find(params[:id])
-    @notes = @user.favorite_notes.order(updated_at: :desc).page(params[:page])
+    # @notes = @user.favorite_notes.order(updated_at: :desc).page(params[:page])
   end
 end
-# .where(is_private: false)
