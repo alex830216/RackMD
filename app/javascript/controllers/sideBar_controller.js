e09c3e19142db7442ee56519a984958a2e71027f
YiHua <helen904526@gmail.com>
2021-09-27 02:09:04 +0800

    add info box

--- app/javascript/controllers/sideBar_controller.js
+++ app/javascript/controllers/sideBar_controller.js
@@ -1,10 +1,17 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "sidebar" ]
+  static targets = [ "sidebar", "infoBox" ]
   static values = { }
 
   toggle(){
-      this.sidebarTarget.classList.toggle("open")
+    this.sidebarTarget.classList.toggle("open")
+  }
+  infoBoxToggle(){
+    if (this.infoBoxTarget.style.display === "none") {
+      this.infoBoxTarget.style.display = "block"
+    } else {
+      this.infoBoxTarget.style.display = "none"
+    }
   }
 }
