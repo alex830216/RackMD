a9512062ea9cc97e28113469a8575ae14dbee099
YiHua <helen904526@gmail.com>
2021-09-27 14:55:03 +0800

    change MD-editor and autosave succeed

--- app/javascript/controllers/autosave_controller.js
+++ app/javascript/controllers/autosave_controller.js
@@ -6,18 +6,26 @@ export default class extends Controller {
   static targets = [ "name" ]
   static values = { id: String }
   initialize(){
-    this.updateValue = debounce(this.updateValue, 500).bind(this)
+    this.updateValue = debounce(this.updateValue, 2000).bind(this)
+  }
+  connect() {
+    console.log("autosave connect!")
   }
   updateValue() {
     const csrfToken = document.querySelector("meta[name=csrf-token]").content;
     axios.defaults.headers.common["X-CSRF-Token"] = csrfToken;
-    // 找到 markdown 的內容，在.ProseMirror 這個 class 內
-    const content = document.querySelector('.ProseMirror').innerHTML
+    // 找到 markdown 的內容，在.CodeMirror-code 這個 class 內
+    const content = document.querySelector(".CodeMirror-code").innerHTML
     // .toastui-editor-contents這個 class 會渲染出 html ，因此在這裡找標題
-    const editorContent = document.querySelector('.toastui-editor-contents')
+    const editorContent = document.querySelector('.md-editor-preview')
     // 確定第一行是不是 h1 格式
-    const title = editorContent.firstChild.nodeName === 'H1' ? editorContent.firstchild.innerHTML : 'Untitled'
+    console.log(`debug innerText=${editorContent.firstChild.innerText}`)
+    const title =
+      editorContent.firstChild.nodeName === "H1"
+        ? editorContent.firstChild.innerText
+        : "Untitled"
     let data = { 'note': { title, content }}
+    console.log(`idValue is ${this.idValue}`)
     axios.put(`/notes/${this.idValue}`,data)
     .then(res => {
       console.log(res.data)
