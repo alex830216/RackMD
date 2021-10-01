1f350e1f7b6ec77dad24b23c76002ca12148fe32
john <t69685186@gmail.com>
2021-10-01 13:07:57 +0800

    修正資料庫

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_023535) do
+ActiveRecord::Schema.define(version: 2021_09_29_093156) do
 
   create_table "collections", force: :cascade do |t|
     t.integer "user_id", null: false
